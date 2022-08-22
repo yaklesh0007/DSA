a9cfde4e6631a6a31649ec460939f939a9a60a6d
yaklesh0007 <yaklesh0007@gmail.com>
2022-08-22 15:11:16 +0545

    added input fields that will help to demonstrate inserting values in array

--- array_insertion.js
+++ array_insertion.js
@@ -1,13 +1,36 @@
-let data=[20,30,40,60,70]
-const newel=50
-const position=3
-for(i=data.length-1;i>=0;i--){
-    console.warn(`${data[i]}`)
-   if(i>=position){
-    data[i+1]=data[i]
-    if(i==position){
-        data[i]=newel
+// let data=[20,30,40,60,70]
+// const newel=50
+// const position=3
+// for(i=data.length-1;i>=0;i--){
+//     console.warn(`${data[i]}`)
+//    if(i>=position){
+//     data[i+1]=data[i]
+//     if(i==position){
+//         data[i]=newel
+//     }
+//    }
+// }
+// console.warn(data)
+
+function insertnewelement() {
+    let items = [120, 221, 220, 210, 300, 240, 600]
+    let newEl = document.getElementById("newEl").value
+    newEl=parseInt(newEl)
+    let Position = document.getElementById("Position").value
+    if (Position <= items.length && typeof parseInt(Position) == "number") {
+        for (i = items.length - 1; i >= 0; i--) {
+            if (i >= Position) {
+                items[i + 1] = items[i]
+                if (i == Position && typeof parseInt(newEl) == "number") {
+                    items[i]=newEl
+                }
+            }
+
+        }
     }
-   }
+    else{
+        alert("please enter valid position and valid element as both need to be numeric")
+    }
+   console.warn(items)
+
 }
-console.warn(data)
