c52b96609d5a2d596acb04457fdcafabc8ef299f
yaklesh0007 <yaklesh0007@gmail.com>
2022-08-22 14:27:37 +0545

    added an element to the array

--- array_insertion.js
+++ array_insertion.js
@@ -3,5 +3,11 @@ const newel=50
 const position=3
 for(i=data.length-1;i>=0;i--){
     console.warn(`${data[i]}`)
-   
+   if(i>=position){
+    data[i+1]=data[i]
+    if(i==position){
+        data[i]=newel
+    }
+   }
 }
+console.warn(data)
